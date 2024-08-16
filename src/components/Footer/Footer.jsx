d025aca68b0d9a0df6911dd4ad8c490b828e2523
Thế Bảo <nbkbvohoc@gmail.com>
2024-08-16 09:55:19 +0700

    to 2h4m47s

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import './Footer.css'
+import { assets } from '../../assets/assets';
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>About Us: Website is proud to bring fresh, safe and high quality food products to every family. We are committed to providing a variety of foods, from regional specialties to familiar dishes, making your meals more rich and nutritious.</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>0933-223-872</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2024 &copy; tomato.com - All Right Reserved</p>
     </div>
-  )
+  );
 }
 
 export default Footer
