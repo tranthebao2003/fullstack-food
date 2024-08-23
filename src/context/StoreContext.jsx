edf543ea4cc0374e9ab59861519d2a3f7ef84f6c
Thế Bảo <nbkbvohoc@gmail.com>
2024-08-23 10:09:17 +0700

    edit url

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
-    const url = 'http://localhost:4000'
+    const url = 'https://fullstack-food-backend.onrender.com'
     const [token, setToken] = useState('')
     const [food_list, setFoodList] = useState([])
 
