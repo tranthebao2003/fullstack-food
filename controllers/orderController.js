316d8700255ee726c47009ed78601aebb8381f6e
Thế Bảo <nbkbvohoc@gmail.com>
2024-08-23 10:32:01 +0700

    edit frontend_url in file orderContriller.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async (req, res) => {
 
     // url fronend
-    const frontend_url = 'http://localhost:5174'
+    const frontend_url = 'https://fullstack-food-frontend.onrender.com/'
 
     try {
         const newOrder = new orderModel({
